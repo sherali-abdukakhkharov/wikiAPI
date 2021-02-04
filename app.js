2050d156e32eccd62227159ba9dc709b9dbdeaef
unknown <sherali.abdukakhkharov@gmail.com>
2021-02-05 01:25:46 +0500

    Developed RESTful API

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-sherali:test123@todolist.govc7.mongodb.net/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useUnifiedTopology: true, useNewUrlParser: true });
 
 const articleSchema = {
     title: String,
@@ -20,18 +20,98 @@ const articleSchema = {
 
 const Article = new mongoose.model("article", articleSchema);
 
+///////////////////////////Requests targeting all articles///////////////////////////
 
-app.get("/articles", function(req, res){
-    Article.find(function(err, articles){
+app.route("/articles")
+
+.get(function(req, res){
+  Article.find(function(err, articles){
+    if(!err){
+      res.send(articles);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.post(function(req, res){
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully add a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+///////////////////////////Requests targeting a specific article///////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found");
+    }
+  });
+})
+
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
       if(!err){
-        res.send(articles);
+        res.send("Successfully updated " + req.params.articleTitle + " article.");
       } else {
         res.send(err);
       }
-      
+  });
+})
 
-    });
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated " + req.params.articleTitle + " article.");
+      } else {
+        res.send(err);
+      }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteOne({title: req.params.articleTitle}, function(err){
+    if(!err){
+      res.send("Successfully deleted " + req.params.articleTitle + " article.");
+    } else {
+      res.send(err);
+    }
+  })
 });
+
+
 
 
 let port = process.env.PORT;
